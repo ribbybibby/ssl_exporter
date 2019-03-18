39c4ef79376fd72cd431408a8de7a69f7d0760c9
Rob Best <robertbest89@gmail.com>
2019-03-18 12:24:28 +0000

    Update and add to tests for client authentication

--- ssl_exporter_test.go
+++ ssl_exporter_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
@@ -10,29 +14,52 @@ import (
 )
 
 func TestProbeHandler(t *testing.T) {
+	certContent, err := ioutil.ReadFile("test/badssl.com-client.pem")
+	if err != nil {
+		t.Fatalf("Can't read test client certificate from disk")
+	}
+
+	keyContent, err := ioutil.ReadFile("test/badssl.com-client-key.pem")
+	if err != nil {
+		t.Fatalf("Can't read test client certificate key from disk")
+	}
+
+	keyBlock, _ := pem.Decode(keyContent)
+
+	keyBlockDecrypted, err := x509.DecryptPEMBlock(keyBlock, []byte("badssl.com"))
+	if err != nil {
+		t.Fatalf("Issue decrypting test client key")
+	}
+
+	emptyRootCAs := x509.NewCertPool()
+
+	certificate, err := tls.X509KeyPair(certContent, keyBlockDecrypted)
 
 	// Test the behaviour of various target URIs
 	//    'ok' denotes whether we expect a succesful https connection
-	//    'insecure' denotes whether we ignore invalid certs
 	cases := []struct {
-		uri      string
-		ok       bool
-		insecure bool
+		uri       string
+		ok        bool
+		tlsConfig *tls.Config
 	}{
 		// Test against an assumed valid, reachable and functioning HTTPS address
-		{uri: "https://google.com", ok: true, insecure: false},
+		{uri: "https://google.com", ok: true, tlsConfig: &tls.Config{}},
 		// Test against a HTTP address
-		{uri: "http://google.com", ok: false, insecure: false},
+		{uri: "http://google.com", ok: false, tlsConfig: &tls.Config{}},
 		// Test against an expired certificate when we're rejecting invalid certs
-		{uri: "https://expired.badssl.com", ok: false, insecure: false},
+		{uri: "https://expired.badssl.com", ok: false, tlsConfig: &tls.Config{}},
 		// Test against an expired certificate when we're accepting invalid certs
-		{uri: "https://expired.badssl.com", ok: true, insecure: true},
+		{uri: "https://expired.badssl.com", ok: true, tlsConfig: &tls.Config{InsecureSkipVerify: true}},
 		// Test against a target with no protocol
-		{uri: "google.com", ok: false, insecure: false},
+		{uri: "google.com", ok: false, tlsConfig: &tls.Config{}},
 		// Test against a string with spaces
-		{uri: "with spaces", ok: false, insecure: false},
+		{uri: "with spaces", ok: false, tlsConfig: &tls.Config{}},
 		// Test against nothing
-		{uri: "", ok: false, insecure: false},
+		{uri: "", ok: false, tlsConfig: &tls.Config{}},
+		// Test with client authentication
+		{uri: "https://client.badssl.com", ok: true, tlsConfig: &tls.Config{Certificates: []tls.Certificate{certificate}}},
+		// Test with an empty root CA bundle
+		{uri: "https://google.com", ok: false, tlsConfig: &tls.Config{RootCAs: emptyRootCAs}},
 	}
 
 	fmt.Println("Note: The error logs in these tests are expected. One of the important tests is that we return the expected body, even in the face of errors.")
@@ -47,7 +74,7 @@ func TestProbeHandler(t *testing.T) {
 
 		rr := httptest.NewRecorder()
 		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			probeHandler(w, r, test.insecure)
+			probeHandler(w, r, test.tlsConfig)
 		})
 
 		handler.ServeHTTP(rr, req)
