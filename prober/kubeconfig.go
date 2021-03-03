d9e08a7963d5cbc7e508e3ec47cac88b573ef60f
Trey Dockendorf <tdockendorf@osc.edu>
2021-03-03 12:47:45 -0500

    Determine relative using filepath.IsAbs

--- prober/kubeconfig.go
+++ prober/kubeconfig.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/ribbybibby/ssl_exporter/config"
@@ -54,7 +53,7 @@ func ProbeKubeconfig(ctx context.Context, target string, module config.Module, r
 			continue
 		}
 		// Path is relative to kubeconfig path
-		if !strings.HasPrefix(c.Cluster.CertificateAuthority, "/") {
+		if !filepath.IsAbs(c.Cluster.CertificateAuthority) {
 			newPath := filepath.Join(filepath.Dir(k.Path), c.Cluster.CertificateAuthority)
 			c.Cluster.CertificateAuthority = newPath
 		}
@@ -64,7 +63,7 @@ func ProbeKubeconfig(ctx context.Context, target string, module config.Module, r
 			continue
 		}
 		// Path is relative to kubeconfig path
-		if !strings.HasPrefix(u.User.ClientCertificate, "/") {
+		if !filepath.IsAbs(u.User.ClientCertificate) {
 			newPath := filepath.Join(filepath.Dir(k.Path), u.User.ClientCertificate)
 			u.User.ClientCertificate = newPath
 		}
