d7c621df556886e0eb124f356735c71dbe02caa9
hroongta <hroongta@tripadvisor.com>
2021-04-21 20:39:46 -0400

    bug fix

--- ssl_exporter.go
+++ ssl_exporter.go
@@ -24,7 +24,7 @@ func probeHandler(w http.ResponseWriter, r *http.Request, conf *config.Config, d
 	moduleName := r.URL.Query().Get("module")
 
 	if moduleName == "" {
-		moduleName = *defaultMod
+		moduleName = defaultMod
 	}
 
 	if moduleName == "" {
@@ -65,7 +65,7 @@ func probeHandler(w http.ResponseWriter, r *http.Request, conf *config.Config, d
 	target := r.URL.Query().Get("target")
 
 	if target == "" {
-		target = *targetCli
+		target = targetCli
 	}
 
 	if target == "" {
@@ -145,7 +145,7 @@ func main() {
 
 	http.Handle(*metricsPath, promhttp.Handler())
 	http.HandleFunc(*probePath, func(w http.ResponseWriter, r *http.Request) {
-		probeHandler(w, r, conf, defaultMod, target)
+		probeHandler(w, r, conf, *defaultMod, *target)
 	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write([]byte(`<html>
