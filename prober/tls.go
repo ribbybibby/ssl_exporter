39c5f93cbcd324d96b473cb0394cc4bf830c7e5d
Ben Ritcey <ben+github@ritcey.com>
2021-11-18 12:01:54 -0500

    Support TLS renegotiation

--- prober/tls.go
+++ prober/tls.go
@@ -18,6 +18,10 @@ func newTLSConfig(target string, registry *prometheus.Registry, pTLSConfig *pcon
 		return nil, err
 	}
 
+	// while generally less-secure, supporting renegotiation for the purposes of
+	// checking certificate expiration is low-risk
+	tlsConfig.Renegotiation = tls.RenegotiateFreelyAsClient
+
 	if tlsConfig.ServerName == "" && target != "" {
 		targetAddress, _, err := net.SplitHostPort(target)
 		if err != nil {
